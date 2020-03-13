8b7bc5b552089a5cf728cad12834d537d26ffa76
unknown <csctest@dnbj-wuyizhe-nb.csc.local>
2020-03-13 13:22:27 +0800

    bootstrap

--- src/main/java/life/majiang/community/controller/HelloController.java
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
@@ -12,6 +11,6 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(@RequestParam(name = "name")String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
